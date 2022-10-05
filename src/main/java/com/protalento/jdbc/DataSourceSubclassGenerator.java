07756936d47df262093a7a3ff9f2f1611c2c84f6
jose-foronda <jose09621@hotmail.com>
2022-10-05 10:19:59 -0500

    Added log for Environment Variable called DATABASE_URL.

--- src/main/java/com/protalento/jdbc/DataSourceSubclassGenerator.java
+++ src/main/java/com/protalento/jdbc/DataSourceSubclassGenerator.java
@@ -22,12 +22,14 @@ public class DataSourceSubclassGenerator {
 		String url_db = System.getenv("url_db");
 		String user_db = System.getenv("user_db");
 		String password_db = System.getenv("password_db");
+		String DATABASE_URL = System.getenv("DATABASE_URL");
 		
 		logger.info("Environment Variable driver_db is... : " +  System.getenv("driver_db"));
 		logger.info("Environment Variable url_db is... : " +  System.getenv("url_db"));
 		logger.info("Environment Variable user_db is... : " +  System.getenv("user_db"));
 		logger.info("Environment Variable password_db is... : " +  System.getenv("password_db"));
-
+		logger.info("Environment Variable DATABASE_URL is... : " +  System.getenv("DATABASE_URL"));
+		
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(url_db, user_db, password_db);
 		driverManagerDataSource.setDriverClassName(driver_db);
 
